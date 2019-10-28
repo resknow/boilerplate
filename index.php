e0485ee26d63e1245a381318366f2b1db6bf7390
Chris Galbraith <chris@resknow.co.uk>
2019-10-28 15:24:51 +0000

    Move core to it's own package

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /**
  * @package Boilerplate
- * @version 4.0.1
+ * @version 4.0.4
  * @author Chris Galbraith
  *
  * This file is used to include everything
@@ -14,56 +14,14 @@
  *
  */
 
-use BP\Controller;
-use BP\CoreTheme;
-use Whoops\Run;
-use Whoops\Handler\PrettyPageHandler;
-
 // Start a Session
 session_start();
 
 // Define Root Directory
 define('ROOT_DIR', __DIR__);
 
-// Define Version
-define('VERSION', '4.0.1');
+$vendor_dir = ROOT_DIR . '/_includes/vendor';
 
 // Load Dependencies
-require_once ROOT_DIR . '/_includes/vendor/autoload.php';
-
-// Load Site Config
-$_config = ( is_readable(ROOT_DIR . '/../.config.yml') ? '/../.config.yml' : '/.config.yml' );
-$_config = Spyc::YAMLLoad(ROOT_DIR . $_config);
-
-// Detect Admin Mode
-if ( array_key_exists( 'admin_mode', $_config ) && $_config['admin_mode'] === true && $_config['environment'] === 'dev' ) {
-    require_once ROOT_DIR . '/_includes/core/ui/setup.php';
-}
-
-// Setup Whoops
-$_whoops = new Run;
-
-// For Development, show errors
-if ( $_config['environment'] == 'dev' ) {
-    $_whoops->prependHandler(new PrettyPageHandler);
-}
-
-$_whoops->register();
-
-// Load Path/URL Setup
-require_once ROOT_DIR . '/_includes/core/bootstrap/url.php';
-
-// Create Theme Object
-$_theme = new CoreTheme('_templates');
-
-// Include functions, classes & plugins
-require_once ROOT_DIR . '/_includes/core/includes.php';
-
-// Action: template.init
-do_action( 'template.init', $_theme );
-
-// Run Router
-router()->run();
-
-// Render the Page
-echo $_theme->render( apply_filters( 'template.render', $_theme->load($_path) ), get() );
+require_once $vendor_dir . '/autoload.php';
+require_once $vendor_dir . '/resknow/boilerplate-core/bootstrap.php';
